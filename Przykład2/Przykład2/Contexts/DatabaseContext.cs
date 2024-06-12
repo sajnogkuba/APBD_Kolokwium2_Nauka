ecb817cd757a7b9b9b1d530d1c9b0a753989c64a
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-06-12 21:10:57 +0200

    add data to tables

--- Przykład2/Przykład2/Contexts/DatabaseContext.cs
+++ Przykład2/Przykład2/Contexts/DatabaseContext.cs
@@ -25,5 +25,123 @@ public class DatabaseContext : DbContext
         
         modelBuilder.Entity<WykonawcaUtwor>()
             .HasKey(wu => new { wu.IdWMuzyk, wu.IdWUtwor });
+
+        modelBuilder.Entity<Muzyk>()
+            .HasData(new List<Muzyk>()
+            {
+                new Muzyk()
+                {
+                    IdMuzyk = 1,
+                    Imie = "Jan",
+                    Nazwisko = "Kowalski",
+                    Pseudonim = "Kowal"
+                },
+                new Muzyk()
+                {
+                    IdMuzyk = 2,
+                    Imie = "Anna",
+                    Nazwisko = "Nowak",
+                    Pseudonim = "Nowak"
+                },
+                new Muzyk()
+                {
+                    IdMuzyk = 3,
+                    Imie = "Piotr",
+                    Nazwisko = "Wiśniewski",
+                    Pseudonim = "Wiśnia"
+                }
+            });
+        
+        modelBuilder.Entity<Utwor>()
+            .HasData(new List<Utwor>()
+            {
+                new Utwor()
+                {
+                    IdUtwor = 1,
+                    NazwaUtworu = "Piosenka1",
+                    CzasTrwania = 3.5f,
+                    IdAlbum = 2
+                },
+                new Utwor()
+                {
+                    IdUtwor = 2,
+                    NazwaUtworu = "Piosenka2",
+                    CzasTrwania = 4.2f,
+                    IdAlbum = 1
+                },
+                new Utwor()
+                {
+                    IdUtwor = 3,
+                    NazwaUtworu = "Piosenka3",
+                    CzasTrwania = 2.8f,
+                    IdAlbum = 1
+                }
+            });
+        
+        modelBuilder.Entity<Wytwornia>()
+            .HasData(new List<Wytwornia>()
+            {
+                new Wytwornia()
+                {
+                    IdWytwornia = 1,
+                    Nazwa = "Sony Music"
+                },
+                new Wytwornia()
+                {
+                    IdWytwornia = 2,
+                    Nazwa = "Universal Music"
+                },
+                new Wytwornia()
+                {
+                    IdWytwornia = 3,
+                    Nazwa = "Warner Music"
+                }
+            });
+        
+        modelBuilder.Entity<Album>()
+            .HasData(new List<Album>()
+            {
+                new Album()
+                {
+                    IdAlbum = 1,
+                    NazwaAlbumu = "Album1",
+                    DataWydania = new DateTime(2021, 1, 1),
+                    IdWytwornia = 1
+                },
+                new Album()
+                {
+                    IdAlbum = 2,
+                    NazwaAlbumu = "Album2",
+                    DataWydania = new DateTime(2021, 2, 1),
+                    IdWytwornia = 2
+                },
+                new Album()
+                {
+                    IdAlbum = 3,
+                    NazwaAlbumu = "Album3",
+                    DataWydania = new DateTime(2021, 3, 1),
+                    IdWytwornia = 3
+                }
+            });
+        
+        modelBuilder.Entity<WykonawcaUtwor>()
+            .HasData(new List<WykonawcaUtwor>()
+            {
+                new WykonawcaUtwor()
+                {
+                    IdWMuzyk = 1,
+                    IdWUtwor = 2
+                },
+                new WykonawcaUtwor()
+                {
+                    IdWMuzyk = 2,
+                    IdWUtwor = 1
+                },
+                new WykonawcaUtwor()
+                {
+                    IdWMuzyk = 3,
+                    IdWUtwor = 3
+                }
+            });
     }
 }
