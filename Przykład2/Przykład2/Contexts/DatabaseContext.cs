e509835afee9fff262a55dd8610a676cbf2e513e
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-06-12 20:50:28 +0200

    create 2nd solution

---
+++ Przykład2/Przykład2/Contexts/DatabaseContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Przykład2.Contexts;
+
+public class DatabaseContext : DbContext
+{
+    protected DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
