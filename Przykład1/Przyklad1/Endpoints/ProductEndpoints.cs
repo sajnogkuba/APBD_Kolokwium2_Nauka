3ee22134cb6662604e3c118993923fac48ce8a67
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-06-12 19:00:43 +0200

    add implementation to product service

--- Przykład1/Przyklad1/Endpoints/ProductEndpoints.cs
+++ Przykład1/Przyklad1/Endpoints/ProductEndpoints.cs
@@ -1,4 +1,6 @@
+using Przyklad1.Context;
 using Przyklad1.Exceptions;
+using Przyklad1.RequestModels;
 using Przyklad1.Services;
 
 namespace Przyklad1.Endpoints;
@@ -8,6 +10,17 @@ public static class ProductEndpoints
     public static void RegisterProductEndpoints(this RouteGroupBuilder builder)
     {
         var group = builder.MapGroup("products");
-        
+        group.MapPost("", async (PostProductRequest data, IProductService service) =>
+        {
+            try
+            {
+                service.AddProduct(data);
+                return Results.Created();
+            }
+            catch (NotFoundException e)
+            {
+                return Results.NotFound(e.Message);
+            }
+        });
     }
 }
--- Przykład1/Przyklad1/Services/ProductService.cs
+++ Przykład1/Przyklad1/Services/ProductService.cs
@@ -1,3 +1,6 @@
+using Przyklad1.Context;
+using Przyklad1.Exceptions;
+using Przyklad1.Models;
 using Przyklad1.RequestModels;
 
 namespace Przyklad1.Services;
@@ -8,10 +11,26 @@ public interface IProductService
 }
 
 
-public class ProductService : IProductService
+public class ProductService(DataBaseContext context) : IProductService
 {
     public void AddProduct(PostProductRequest data)
     {
-        throw new NotImplementedException();
+        var newProduct = new Product
+        {
+            ProductName = data.productName,
+            ProductWeight = data.productWeight,
+            ProductHeight = data.productHeight,
+            ProductDepth = data.productDepth
+        };
+        
+        context.Products.Add(newProduct);
+        context.ProductsCategories.AddRange(data.productCategories.Select(e => new ProductCategory
+        {
+            Product = newProduct,
+            Category = context.Categories.FirstOrDefault(category => category.CategoryID == e) 
+                       ?? throw new NotFoundException($"Category with id:{e} does not exist")
+        }));
+        
+        context.SaveChanges();
     }
 }
