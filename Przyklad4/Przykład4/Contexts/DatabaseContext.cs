1366c4123a35692d22866cd4b65d19441318b090
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-06-13 17:32:15 +0200

    add test data

--- Przyklad4/Przykład4/Contexts/DatabaseContext.cs
+++ Przyklad4/Przykład4/Contexts/DatabaseContext.cs
@@ -34,5 +34,118 @@ public class DatabaseContext : DbContext
             backpack.BackpackCharacterId,
             backpack.BackpackItemId
         });
+
+        modelBuilder.Entity<Item>().HasData(new List<Item>()
+        {
+            new Item()
+            {
+                ItemId = 1,
+                ItemName = "TestItem",
+                ItemWeight = 10
+            },
+            new Item()
+            {
+                ItemId = 2,
+                ItemName = "TestItem2",
+                ItemWeight = 20
+            },
+            new Item()
+            {
+                ItemId = 3,
+                ItemName = "TestItem3",
+                ItemWeight = 30
+            }
+        });
+        
+        modelBuilder.Entity<Character>().HasData(new List<Character>()
+        {
+            new Character()
+            {
+                CharacterId = 1,
+                CharacterFirstName = "TestCharacterFirstName",
+                CharacterLastName = "TestCharacterLastName",
+                CharacterCurrentWeight = 0,
+                CharacterMaxWeight = 100
+            },
+            new Character()
+            {
+                CharacterId = 2,
+                CharacterFirstName = "TestCharacterFirstName2",
+                CharacterLastName = "TestCharacterLastName2",
+                CharacterCurrentWeight = 20,
+                CharacterMaxWeight = 120
+            },
+            new Character()
+            {
+                CharacterId = 3,
+                CharacterFirstName = "TestCharacterFirstName3",
+                CharacterLastName = "TestCharacterLastName3",
+                CharacterCurrentWeight = 30,
+                CharacterMaxWeight = 130
+            }
+        });
+        
+        modelBuilder.Entity<Backpack>().HasData(new List<Backpack>()
+        {
+            new Backpack()
+            {
+                BackpackCharacterId = 1,
+                BackpackItemId = 1,
+                BackpackAmount = 1
+            },
+            new Backpack()
+            {
+                BackpackCharacterId = 2,
+                BackpackItemId = 2,
+                BackpackAmount = 2
+            },
+            new Backpack()
+            {
+                BackpackCharacterId = 3,
+                BackpackItemId = 3,
+                BackpackAmount = 3
+            }
+        });
+        
+        modelBuilder.Entity<Title>().HasData(new List<Title>()
+        {
+            new Title()
+            {
+                TitleId = 1,
+                TitleName = "TestTitle"
+            },
+            new Title()
+            {
+                TitleId = 2,
+                TitleName = "TestTitle2"
+            },
+            new Title()
+            {
+                TitleId = 3,
+                TitleName = "TestTitle3"
+            }
+        });
+        
+        modelBuilder.Entity<CharacterTitle>().HasData(new List<CharacterTitle>()
+        {
+            new CharacterTitle()
+            {
+                CharacterId = 1,
+                TitleId = 1,
+                AcquiredAt = DateTime.Now
+            },
+            new CharacterTitle()
+            {
+                CharacterId = 2,
+                TitleId = 2,
+                AcquiredAt = DateTime.Now
+            },
+            new CharacterTitle()
+            {
+                CharacterId = 3,
+                TitleId = 3,
+                AcquiredAt = DateTime.Now
+            }
+        });
     }
 }
